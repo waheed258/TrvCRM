04db2bba3d1f1d6440cb1a1b498ac2d58ce3bca4
saipramodbalantrapu <saipramod.balatrapu@dinoosys.com>
2018-04-11 22:58:01 +0530

    New Solution added

---
+++ TravelCRM/App_Code/Entities/consultantEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for consultantEntity
+/// </summary>
+public class consultantEntity
+{
+
+    public int ConsultantID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Mobile { get; set; }    
+    public string Email { get; set; }
+    public string LoginID { get; set; }
+    public string Password { get; set; }
+    public int Designation { get; set; }
+    public int Branch { get; set; }   
+    public int Status { get; set; }
+    public string Image { get; set; }   
+    public int? UpdatedBy { get; set; }
+
+}
---
+++ TravelCRM/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelCRM.Startup))]
+namespace TravelCRM
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
